6ca97c78b964758ba3a9b220e95fb28f4136f9ad
Priti <61485216+pritiranga@users.noreply.github.com>
2022-08-24 13:36:08 +0530

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,14 +1,6 @@
-terraform{
-    backend "s3" {
-        bucket = "davoclock-aws-cicd-pipeline"
-        encrypt = true
-        key = "terraform.tfstate"
-        region = "us-east-1"
-    }
-}
 
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAV2WNYWNWBUC7BH4R"
-    secret_key = "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"
+    access_key = "AKIAV2WNYWNWEMELEEMC"
+    secret_key = "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"
 }
